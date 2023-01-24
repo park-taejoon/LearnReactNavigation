7ae14633fed796458858bf769e8ded727c0420aa
pyh1987 <pyh1987@naver.com>
2023-01-24 22:59:17 +0900

    add

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>;
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,18 @@
+echo "# LearnReactNavigation" >> README.md
+
+# LearnReactNavigation
+
+npx react-native init LearnReactNavigation
+
+# navigation dependencie add
+
+yarn add @react-navigation/native
+
+# react-navigation dependencies library add
+
+yarn add react-native-screens react-native-safe-area-context
+
+# install after
+
+cd ios
+pod install

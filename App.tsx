61220ff40f450cd2133f353e9be15ed74ddb190f
pyh1987 <pyh1987@naver.com>
2023-01-24 23:31:52 +0900

    init start

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detaile" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Button title="detail 열기" />
+    </View>
+  );
+}
+export default HomeScreen;
